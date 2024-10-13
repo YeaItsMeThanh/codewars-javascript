18ed7071d680f1337d576593959242588eb3ad3c
Thanh <yeaitsmethanh@outlook.com>
2024-10-13 04:59:05 -0400

    8kyu(js): add "Convert a String to a Number!"

---
+++ app/8kyu/convert-a-string-to-a-number.js
@@ -0,0 +1,1 @@
+export const stringToNumber = str => +str
---
+++ test/8kyu/convert-a-string-to-a-number.js
@@ -0,0 +1,12 @@
+import { assert } from 'chai'
+
+import { stringToNumber } from '../../app/8kyu/convert-a-string-to-a-number.js'
+
+describe('stringToNumber', function () {
+    it('should work for the examples', function () {
+        assert.strictEqual(stringToNumber('1234'), 1234)
+        assert.strictEqual(stringToNumber('605'), 605)
+        assert.strictEqual(stringToNumber('1405'), 1405)
+        assert.strictEqual(stringToNumber('-7'), -7)
+    })
+})
