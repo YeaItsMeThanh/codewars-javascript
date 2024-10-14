0175084523caf84e464842a0f3701fcad57e64ac
Thanh <yeaitsmethanh@outlook.com>
2024-10-14 04:42:46 -0400

    7kyu(js): refactor "Vowel Count"

--- test/7kyu/vowel-count.js
+++ test/7kyu/vowel-count.js
@@ -1,6 +1,6 @@
 import { assert } from 'chai'
 
-import { getCount } from '../../app/8kyu/vowel-count.js'
+import { getCount } from '../../app/7kyu/vowel-count.js'
 
 describe('Vowels Count Tests', function () {
     it("should return 5 for 'abracadabra'", function () {
