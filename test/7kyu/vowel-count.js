dd306b08c07f6fb1ac8077f33283e9b3ceb8bd31
Thanh <yeaitsmethanh@outlook.com>
2024-10-14 04:51:06 -0400

    7kyu(js): refactor "Vowel Count" a bit more

--- test/7kyu/vowel-count.js
+++ test/7kyu/vowel-count.js
@@ -2,8 +2,8 @@ import { assert } from 'chai'
 
 import { getCount } from '../../app/7kyu/vowel-count.js'
 
-describe('Vowels Count Tests', function () {
-    it("should return 5 for 'abracadabra'", function () {
+describe('Vowels Count Tests', () => {
+    it("should return 5 for 'abracadabra'", () => {
         assert.strictEqual(getCount('abracadabra'), 5)
     })
 })
